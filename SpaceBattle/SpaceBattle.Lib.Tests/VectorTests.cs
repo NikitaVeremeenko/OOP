4f24c89100173a6e8afdfc834e63881b47e31137
ceyaa <nikitaveremeenko03@gmail.com>
2024-12-17 23:40:37 +0600

    IMovable, Vector, MoveCommand, Tests

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/VectorTests.cs
@@ -0,0 +1,20 @@
+﻿namespace SpaceBattle.Lib.Tests;
+
+public class VectorTests
+{
+    [Fact]
+    public void Vector_GetHashCode()
+    {
+        var arr = new int[] { 1, 5 };
+        var test = new Vector(arr);
+        Assert.Equal(arr.GetHashCode(), test.GetHashCode());
+    }
+
+    [Fact]
+    public void Vector_Equals_PassNotVectorType()
+    {
+        var num = 2;
+        var test = new Vector(new int[] { 1, 2 });
+        Assert.True(!test.Equals(num));
+    }
+}
